c7537e9cb726f154b3b9c255e75ba2b400aee844
Maaaikdev <59856515+maaaikdev@users.noreply.github.com>
2025-05-16 16:14:25 -0500

    Update Find-the-smallest-integer-in-the-array.js

--- Find-the-smallest-integer-in-the-array.js
+++ Find-the-smallest-integer-in-the-array.js
@@ -7,3 +7,17 @@ function findSmallestInt(arr) {
 findSmallestInt([34, 15, 88, 2]);
 
 //Option 2
+
+function findSmallestInt(arr) {
+  //your code here
+  console.log(arr);
+  let smallest = arr[0]; // Supone que el primero es el menor
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i]; // Actualiza si encuentra uno más pequeño
+    }
+  }
+  return smallest;
+}
+
+console.log(findSmallestInt([34, 15, 88, 2]));
