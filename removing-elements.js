7ed9e93698c531f2c05ec4731d6c497b8286886f
Maaaikdev <59856515+maaaikdev@users.noreply.github.com>
2025-05-16 16:38:24 -0500

    Create removing-elements.js

---
+++ removing-elements.js
@@ -0,0 +1,11 @@
+function removeEveryOther(arr) {
+  //your code here
+  let result = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    result.push(arr[i]);
+  }
+
+  return result;
+}
+
+console.log(removeEveryOther(['Keep', 'Remove', 'Keep', 'Remove', 'Keep']));
